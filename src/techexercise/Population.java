3653b2ef75f47a8e9e4e6ab753fbf22ae8e698c6
MPPilon <MPPilon@gmail.com>
2017-03-17 11:27:51 -0400

    Add Population and Specifics interfaces

---
+++ src/techexercise/Population.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techexercise;
+
+/**
+ *
+ * @author c0538434
+ */
+public interface Population {
+    //Each animal must have a growth rate
+    double annualGrowthRate();
+    //Must also be able to calculate population after a number of years
+    int populationAfterYears(int years);
+}
---
+++ src/techexercise/Specifics.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techexercise;
+
+/**
+ *
+ * @author c0538434
+ */
+public interface Specifics {
+    //Each animal must have a birth type and method of locomotion
+    String birthType();
+    String propulsionType();
+}
